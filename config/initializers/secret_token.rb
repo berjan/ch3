673ae0c7b26370eaa8cefe26aa2f06e40846a36a
Berjan Bruens <berjan@bruens.it>
2014-02-18 17:19:23 +0100

    Initial commit setup ch3 tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ch3App::Application.config.secret_key_base = 'f0ac6a07c943a979945b378e036660b95efa7a5831babc379d11ddac7bbba09c3e3cb06a575286dd074103ac523a9642ec5f6c0bc2b9b011f9485596c6ab0999'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ch3App::Application.config.session_store :cookie_store, key: '_ch3_app_session'
